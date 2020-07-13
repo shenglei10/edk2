06cad4b27a9ff1e802fe6287d918db061f4ee4b6
Shenglei Zhang <shenglei.zhang@intel.com>
2020-07-13 11:14:36 +0800

    New files license check
    Change-Id: Id6d927936a5d2a0b835fc52a294d5910ad9f36f4
    Signed-off-by: Shenglei Zhang <shenglei.zhang@intel.com>

---
+++ FatPkg/FatPei/AAA.c
@@ -0,0 +1,15 @@
+/** @file
+  General purpose supporting routines for FAT recovery PEIM
+
+Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+
+SPDX-License-Identifier: MIT
+
+**/
+
+#include "FatLitePeim.h"
+
+
+#define CHAR_FAT_VALID  0x01
+
+
---
+++ FatPkg/FatPei/BBB.h
@@ -0,0 +1,12 @@
+/** @file
+  FAT format data structures
+
+Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
+
+SPDX-License-Identifier: 
+
+**/
+
+#ifndef _FAT_FMT_H_
+#define _FAT_FMT_H_
+
